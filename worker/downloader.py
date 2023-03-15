c7941e2d7d82e41d7a6a843ea9fc240194e7b821
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-15 15:47:31 +0100

    feat(worker): add basic webpage processing

---
+++ worker/downloader.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_page(url: str) -> bytes:
+    return requests.get(url).content
+
+def extract(page: bytes) -> str:
+    soup = BeautifulSoup(page, "html.parser")
+    return soup.title.text
--- worker/main.py
+++ worker/main.py
@@ -1,14 +1,17 @@
 from rocketry import Rocketry
 from rocketry.conds import every, after_success
 from rocketry.args import Return
+import downloader
 
-scheduler: Rocketry = Rocketry()
+scheduler = Rocketry()
 
 @scheduler.task(every("5 minutes"))
-def do_check() -> any:
-    print("Check")
+def do_check() -> str:
+    print("Downloading…")
+    return downloader.get_page('https://aeon.co/essays/on-nonconformism-or-why-we-need-to-be-seen-and-not-herded')
 
 @scheduler.task(after_success(do_check))
-def do_process(arg = Return('do_check')) -> any:
-    print("Process")
-
+def do_process(page: bytes = Return('do_check')) -> None:
+    print("Processing")
+    title = downloader.extract(page)
+    print(title)
