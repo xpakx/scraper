2a0ff56e3d59dd846f1710a619ead0c384781c38
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-19 11:31:31 +0100

    chore(api): add cors settings

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from consumer import Consumer
 import repository
 
+origins = [
+    "http://localhost",
+    "http://localhost:4200"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 consumer = Consumer()
 consumer.connect('localhost', 5672)
 consumer.start()
