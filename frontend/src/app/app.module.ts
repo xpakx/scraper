75c624ced75184ea1bc6616fd403cf70ba80ebaf
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-21 13:34:45 +0100

    feat(frontend/ui): make better ui

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ActivityComponent } from './activity/activity.component';
 import { ActivitiesComponent } from './activities/activities.component';
 import { StreetsComponent } from './streets/streets.component';
-import { StreetComponent } from './street/street.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ActivityComponent,
     ActivitiesComponent,
-    StreetsComponent,
-    StreetComponent
+    StreetsComponent
   ],
   imports: [
     BrowserModule,
