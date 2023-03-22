3889bf6137c10ca491ea2917cddac1beaf83b5d9
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-22 10:54:13 +0100

    test(worker): test extracting while nothing to extract

--- worker/test/test_downloader.py
+++ worker/test/test_downloader.py
@@ -1,7 +1,13 @@
 from app.downloader import CityStridesDownloader
 from test.resolver import EmptyPropertyResolver
 
-def test_extract():
+def test_extract_should_return_result():
     downloader = CityStridesDownloader(EmptyPropertyResolver())
     output = downloader.extract("<html><body><div class='text-gray-500'>Test</div></body></html>")
     assert 'Test' in output
+
+def test_extract_should_return_none():
+    downloader = CityStridesDownloader(EmptyPropertyResolver())
+    output = downloader.extract("<html><body><div>Test</div></body></html>")
+    assert output == None
+    
