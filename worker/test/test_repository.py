7357cdda9b31872080470d5f8f41017a0b037f4e
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-22 12:10:57 +0100

    test(worker): test repository

--- worker/test/test_repository.py
+++ worker/test/test_repository.py
@@ -4,4 +4,19 @@ import unittest
 from unittest.mock import patch
 
 class TestRepository(unittest.TestCase):
-    pass
+    def test_test_changes__if_no_changes(self):
+        repo = app.repository.PageRepository('sqlite:///:memory:')
+        url = "https://example.com"
+        title = "Title 1"
+        assert repo.test_changes(url, title)
+        assert not repo.test_changes(url, title)
+
+    def test_test_changes__if_page_changed(self):
+        repo = app.repository.PageRepository('sqlite:///:memory:')
+        url = "https://example.com"
+        title = "Title 1"
+        changed_title = "Title 2"
+        assert repo.test_changes(url, title)
+        assert repo.test_changes(url, changed_title)
+
+    
