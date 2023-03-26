aab544e5038e059e3529eb0e820d08e05fde1782
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-26 17:33:57 +0200

    chore(utils): get names of streets for each area from OSM

--- utils/data_scraper.py
+++ utils/data_scraper.py
@@ -15,15 +15,35 @@ def getAreas(url: str) -> List[str]:
     return column_data
 
 def getAreasFromFile(name: str) -> List[str]:
-    with open(f'{name}.json', 'r', encoding='utf-8') as f:
+    with open(f'data/{name}.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
 def save_obj(data, name: str) -> None:
-    with open(f'{name}.json', 'w', encoding='utf-8') as f:
+    with open(f'data/{name}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False)
 
+def get_streets_for_area(name: str) -> List[str]:
+    overpass_query = f'''
+        [out:json];
+        area[name="Osiedle {name}"];
+        way(area)["highway"~"^(primary|secondary|tertiary|residential)$"][name];
+        out body;
+    '''
+    response = requests.post("https://overpass-api.de/api/interpreter", data=overpass_query.encode('utf-8'))
+    streets = []
+    for street in response.json()["elements"]:
+        if street['tags'] and street['tags']['name']:
+            streets.append(street['tags']['name'])
+    return list(set(streets))
 
+
+'''
 areas = getAreas('https://pl.wikipedia.org/wiki/Podzia%C5%82_administracyjny_Wroc%C5%82awia')
 save_obj(areas, 'areas')
+'''
 
+areas = getAreasFromFile('areas')
 
+for area in areas:
+    streets = get_streets_for_area(area)
+    save_obj(streets, area.replace(' ', '_').lower())
