7da7530ad49361c41c5139777a3cfff6890d6403
xpakx <55586470+xpakx@users.noreply.github.com>
2023-03-26 18:19:22 +0200

    chore(utils): change scraping to resulting file being more appropriate for new feature

--- utils/data_scraper.py
+++ utils/data_scraper.py
@@ -4,12 +4,10 @@ from typing import List
 import json
 import logging
 
-
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def getAreas(url: str) -> List[str]:
+def get_areas(url: str) -> List[str]:
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     table = soup.find('table', class_='wikitable')
@@ -20,7 +18,7 @@ def getAreas(url: str) -> List[str]:
             column_data.append(cells[0].get_text().strip())
     return column_data
 
-def getListFromFile(name: str) -> List[str]:
+def get_list_from_file(name: str) -> List[str]:
     with open(f'data/{name}.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
@@ -42,22 +40,52 @@ def get_streets_for_area(name: str) -> List[str]:
             streets.append(street['tags']['name'])
     return list(set(streets))
 
-logger.info("Getting areas from Wikipedia…")
-areas = getAreas('https://pl.wikipedia.org/wiki/Podzia%C5%82_administracyjny_Wroc%C5%82awia')
+def get_area_data():
+    logger.info("Getting areas from Wikipedia…")
+    areas = get_areas('https://pl.wikipedia.org/wiki/Podzia%C5%82_administracyjny_Wroc%C5%82awia')
 
-areas_result = []
-streets_result = []
+    areas_result = []
+    streets_result = []
 
+    logger.info("Collecting street data with overpass API…")
+    for area in areas:
+        logger.info(f"Collecting data for {area}…")
+        streets = get_streets_for_area(area)
+        areas_result.append({'area': area, 'streets': streets, 'street_count': len(streets)})
+        streets_result.extend(streets)
 
-logger.info("Collecting street data with overpass API…")
-for area in areas:
-    logger.info(f"Collecting data for {area}…")
-    streets = get_streets_for_area(area)
-    areas_result.append({'area': area, 'streets': streets})
-    streets_result.extend(streets)
+    logger.info("Saving data set…")
+    streets_result = list(set(streets_result))
+    result = {'areas': areas_result, 'streets': streets_result, 'street_count': len(streets_result)}
+    save_obj(result, 'dataset')
 
+def get_street_data():
+    logger.info("Getting areas from Wikipedia…")
+    areas = get_areas('https://pl.wikipedia.org/wiki/Podzia%C5%82_administracyjny_Wroc%C5%82awia')
 
-logger.info("Saving data set…")
-streets_result = list(set(streets_result))
-result = {'areas': areas_result, 'streets': streets_result, 'street_count': len(streets_result)}
-save_obj(result, 'dataset')
+    areas_result = []
+    streets_result = []
+
+    logger.info("Collecting street data with overpass API…")
+    for area in areas:
+        logger.info(f"Collecting data for {area}…")
+        streets = get_streets_for_area(area)
+        areas_result.append({'area': area, 'street_count': len(streets)})
+        for street in streets:
+            street_from_result = None
+            for str in streets_result:
+                if(street == str['name']):
+                    street_from_result = str
+                    break
+            if(street_from_result is None):
+                streets_result.append({'name': street, 'areas' : [area]})
+            else:
+                street_from_result['areas'].append(area)
+
+    areas_result.append({'area': 'Wrocław', 'street_count': len(streets_result)})
+
+    logger.info("Saving data set…")
+    result = {'areas': areas_result, 'streets': streets_result}
+    save_obj(result, 'dataset_streets')
+
+get_street_data()
